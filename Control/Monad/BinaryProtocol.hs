3dcdaec7880795f3b3c186866949ee1878f71f85
Gregory Crosswhite <gcross@phys.washington.edu>
2009-09-04 13:05:06 -0700

    Polished documentation.

--- Control/Monad/BinaryProtocol.hs
+++ Control/Monad/BinaryProtocol.hs
@@ -23,21 +23,28 @@ import System.IO
 
 type BinaryProtocol = StateT (Handle,Handle,L.ByteString) IO
 
--- | Take a BinaryProtocol monad and run it on the given file handle.
--- | PLEASE NOTE: The handle is essentially consumed by this function, so it should not be used afterward.
+-- | Take a BinaryProtocol monad and run it on the given handles for
+--   respectively reading and writing.  (The two given handles are
+--   allowed to be the same if the same handle is used for reading and
+--   writing.)
+--
+-- Note: We run L.hGetContents on the read handle, so don't expect to
+-- be able to use it after you have called this function.
 runProtocol :: BinaryProtocol a -> Handle -> Handle -> IO a
 runProtocol protocol read_handle write_handle = do
     input <- L.hGetContents read_handle
     result <- evalStateT protocol (read_handle,write_handle,input)
--- Note that we deliberately do NOT close the read_handle since result is lazy and hence might
--- need to read more data from the read_handle at a later point.  It will be closed automatically
--- on this side anyway once all of the data has been read.
+-- Note that we deliberately do NOT close the read_handle since result
+-- is lazy and hence might need to read more data from the read_handle
+-- at a later point.  It will be closed automatically on this side
+-- anyway once all of the data has been read.
     if (read_handle /= write_handle)
       then hClose write_handle
       else hFlush write_handle
     return result
 
--- | Read in a value of type 'a' from the connection;  'a' must be an instance of the @Binary@ class.
+-- | Read in a value of type @a@ from the connection; @a@ must be an
+--   instance of the @Binary@ class.
 receive :: Binary a => BinaryProtocol a
 receive = do
     (read_handle,write_handle,input) <- get
@@ -45,13 +52,19 @@ receive = do
     put (read_handle,write_handle,remaining_input)
     return value
 
--- | Send a value of type 'a' down the connection;  'a' must be an instance of the @Binary@ class.
+-- | Send a value of type @a@ down the connection; @a@ must be an
+--   instance of the @Binary@ class.
 send :: Binary a => a -> BinaryProtocol ()
 send value = do
     (_,write_handle,_) <- get
     liftIO $ L.hPut write_handle (B.encode value)
 
--- | Flush the connection.
+-- | Flush buffered send data down the connection.
+--
+-- Note: You need to make sure to call this between sending requests
+-- and receiving responses in order to ensure that the request has
+-- actually been sent down the connection; otherwise you might get
+-- stuck waiting for a response that will not come.
 flush :: BinaryProtocol ()
 flush = do
     (_,write_handle,_) <- get
